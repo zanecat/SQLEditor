599a7680ee68d68e03b92455932c80f5d04fcc21
Dongyu Zhao <dy.zhao@outlook.com>
2017-06-27 19:03:52 +1000

    create angular project

--- js/languageServices/hive/completer/completer.ts
+++
@@ -1,9 +0,0 @@
-// importScripts("../parser/sqlParseSupport.js")
-// importScripts("../parser/sql.js")
-// importScripts("../parser/sqlFunctions.js")
-// export class Completer {
-//     output:string;
-//     constructor(private parser) {
-//         this.output = sql.parseSql("select *","from", "hive", false)
-//     }
-// }
